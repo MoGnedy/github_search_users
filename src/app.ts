7f2a12c2a1d8421db505e1cd03e52de366a7c8c9
Mohamed Gnedy <mognedy@gmail.com>
2022-10-23 23:16:01 +0200

    Use Github users routes in the app and handle some errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import helmet from "helmet";
+import { GraphqlResponseError } from "@octokit/graphql";
+import { router as githubUsersRouter } from "./routes/GithubUsersRoutes";
 
 export const app = express();
 
@@ -16,6 +18,8 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/api/v1/github", githubUsersRouter);
+
 app.use(errorHandler);
 
 function errorHandler(
@@ -28,5 +32,14 @@ function errorHandler(
     return next(err);
   }
 
-  res.sendStatus(500);
+  if (err instanceof GraphqlResponseError) {
+    res.status(400).json({
+      errors: err.errors.map((error) => ({
+        key: error.type,
+        message: error.message,
+      })),
+    });
+  } else {
+    res.sendStatus(500);
+  }
 }
